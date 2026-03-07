45bb4efcc6918c916722859e7ad0dc41f9414238
Akiicodes <akshit.sinha1356@gmail.com>
2026-03-07 13:15:25 +0530

    Day 35 solution

--- data/solutions.ts
+++ data/solutions.ts
@@ -3111,6 +3111,113 @@ int calculate(char* s) {
     return result;
 }
 `
+  },
+  35: {
+    github: `/*Problem: Queue Using Array - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n (number of elements)
+- Second line: n space-separated integers
+
+Output:
+- Print queue elements from front to rear, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+
+Explanation:
+Use array and front/rear pointers. Enqueue inserts at rear, dequeue removes from front. Display from front to rear.*/
+//Solution:
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+
+    struct Node *front = NULL, *rear = NULL, *newNode;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &x);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = x;
+        newNode->next = NULL;
+
+        if(front == NULL) {
+            front = rear = newNode;
+        } else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+
+    struct Node* temp = front;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
+`,
+    leetcode: `#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    int inStack[1000];
+    int outStack[1000];
+    int topIn;
+    int topOut;
+} MyQueue;
+
+MyQueue* myQueueCreate() {
+    MyQueue* obj = (MyQueue*)malloc(sizeof(MyQueue));
+    obj->topIn = -1;
+    obj->topOut = -1;
+    return obj;
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+    obj->inStack[++obj->topIn] = x;
+}
+
+void move(MyQueue* obj) {
+    if(obj->topOut == -1) {
+        while(obj->topIn != -1) {
+            obj->outStack[++obj->topOut] = obj->inStack[obj->topIn--];
+        }
+    }
+}
+
+int myQueuePop(MyQueue* obj) {
+    move(obj);
+    return obj->outStack[obj->topOut--];
+}
+
+int myQueuePeek(MyQueue* obj) {
+    move(obj);
+    return obj->outStack[obj->topOut];
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+    return (obj->topIn == -1 && obj->topOut == -1);
+}
+
+void myQueueFree(MyQueue* obj) {
+    free(obj);
+}
+`
   }
 
 }
