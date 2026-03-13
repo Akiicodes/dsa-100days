062d035567c7e3fb3d80d6ecc3f0a515a8ff38ec
Akiicodes <akshit.sinha1356@gmail.com>
2026-03-13 15:50:49 +0530

    Day 41 Solution

--- data/solutions.ts
+++ data/solutions.ts
@@ -4021,5 +4021,106 @@ int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize)
     return ans;
 }
 `
+  },
+  41: {
+    github: `/*Problem Statement:
+Implement a Queue using a linked list supporting enqueue and dequeue operations.
+
+Input Format:
+- First line contains integer N
+- Next N lines contain queue operations
+
+Output Format:
+- Print dequeued elements
+- Print -1 if dequeue is attempted on an empty queue*/
+//Solution:
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node *front = NULL, *rear = NULL;
+
+void enqueue(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = NULL;
+
+    if(rear == NULL) {
+        front = rear = newNode;
+    } else {
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+int dequeue() {
+    if(front == NULL)
+        return -1;
+
+    struct Node* temp = front;
+    int val = temp->data;
+
+    front = front->next;
+    if(front == NULL)
+        rear = NULL;
+
+    free(temp);
+    return val;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    while(n--) {
+        char op[20];
+        scanf("%s", op);
+
+        if(strcmp(op, "enqueue") == 0) {
+            int x;
+            scanf("%d", &x);
+            enqueue(x);
+        }
+        else if(strcmp(op, "dequeue") == 0) {
+            printf("%d\n", dequeue());
+        }
+    }
+
+    return 0;
+}
+`,
+    leetcode: `#include <stdlib.h>
+
+int leastInterval(char* tasks, int tasksSize, int n) {
+    
+    int freq[26] = {0};
+
+    for(int i = 0; i < tasksSize; i++)
+        freq[tasks[i] - 'A']++;
+
+    int maxFreq = 0;
+
+    for(int i = 0; i < 26; i++)
+        if(freq[i] > maxFreq)
+            maxFreq = freq[i];
+
+    int maxCount = 0;
+
+    for(int i = 0; i < 26; i++)
+        if(freq[i] == maxFreq)
+            maxCount++;
+
+    int partCount = maxFreq - 1;
+    int partLength = n + 1;
+    int result = partCount * partLength + maxCount;
+
+    return result > tasksSize ? result : tasksSize;
+}
+`
   }
  }
