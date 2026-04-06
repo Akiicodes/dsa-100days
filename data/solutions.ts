3e1c2b76e4454b1bd7ea3ff45eec0e9ce0d72e9b
Akiicodes <akshit.sinha1356@gmail.com>
2026-04-06 11:37:04 +0530

    Day 65 Solution

--- data/solutions.ts
+++ data/solutions.ts
@@ -6751,6 +6751,132 @@ int main() {
     return minutes;
 }`
   },
+  65: {
+    github: `/*Problem: Using DFS and parent tracking, detect if undirected graph has a cycle.
+
+Output:
+- YES or NO*/
+//Solution:
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int dfs(struct Node* adj[], int visited[], int curr, int parent) {
+    visited[curr] = 1;
+
+    struct Node* temp = adj[curr];
+    while (temp) {
+        int v = temp->vertex;
+
+        if (!visited[v]) {
+            if (dfs(adj, visited, v, curr))
+                return 1;
+        } else if (v != parent) {
+            return 1;
+        }
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
+int main() {
+    int n, m;
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    struct Node* adj[n];
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+
+        newNode = createNode(u);
+        newNode->next = adj[v];
+        adj[v] = newNode;
+    }
+
+    int visited[n];
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    int hasCycle = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            if (dfs(adj, visited, i, -1)) {
+                hasCycle = 1;
+                break;
+            }
+        }
+    }
+
+    if (hasCycle)
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}`,
+    GeekForGeeks: `class Solution {
+public:
+    bool dfs(int node, int parent, vector<int> adj[], vector<int>& visited) {
+        visited[node] = 1;
+
+        for (auto it : adj[node]) {
+            if (!visited[it]) {
+                if (dfs(it, node, adj, visited))
+                    return true;
+            }
+            else if (it != parent) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        vector<int> adj[V];
+
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> visited(V, 0);
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (dfs(i, -1, adj, visited))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};`
+  },
   
   
   
