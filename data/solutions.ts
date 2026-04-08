bf1bbe23d43d2fb755608e049ba2cff798e570ec
Akiicodes <akshit.sinha1356@gmail.com>
2026-04-08 18:28:06 +0530

    Day 67 Solution

--- data/solutions.ts
+++ data/solutions.ts
@@ -6986,6 +6986,116 @@ bool canFinish(int numCourses, int** prerequisites, int prerequisitesSize, int* 
     return true;
 }`
   },
+  67: {
+    github: `/*Problem: Print topological ordering of a Directed Acyclic Graph (DAG) using DFS.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int v;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->v = v;
+    node->next = NULL;
+    return node;
+}
+
+void dfs(struct Node* adj[], int visited[], int stack[], int* top, int node) {
+    visited[node] = 1;
+
+    struct Node* temp = adj[node];
+    while (temp) {
+        if (!visited[temp->v])
+            dfs(adj, visited, stack, top, temp->v);
+        temp = temp->next;
+    }
+
+    stack[(*top)++] = node;
+}
+
+int main() {
+    int V, E;
+    scanf("%d %d", &V, &E);
+
+    struct Node* adj[V];
+    for (int i = 0; i < V; i++)
+        adj[i] = NULL;
+
+    int u, v;
+    for (int i = 0; i < E; i++) {
+        scanf("%d %d", &u, &v);
+
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+    }
+
+    int visited[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = 0;
+
+    int stack[V], top = 0;
+
+    for (int i = 0; i < V; i++) {
+        if (!visited[i])
+            dfs(adj, visited, stack, &top, i);
+    }
+
+    for (int i = top - 1; i >= 0; i--)
+        printf("%d ", stack[i]);
+
+    return 0;
+}`,
+    leetcode: `int* findOrder(int numCourses, int** prerequisites, int prerequisitesSize, int* prerequisitesColSize, int* returnSize) {
+    int** adj = (int**)malloc(numCourses * sizeof(int*));
+    int* adjSize = (int*)calloc(numCourses, sizeof(int));
+    int* indegree = (int*)calloc(numCourses, sizeof(int));
+
+    for (int i = 0; i < numCourses; i++)
+        adj[i] = (int*)malloc(numCourses * sizeof(int));
+
+    for (int i = 0; i < prerequisitesSize; i++) {
+        int a = prerequisites[i][0];
+        int b = prerequisites[i][1];
+        adj[b][adjSize[b]++] = a;
+        indegree[a]++;
+    }
+
+    int* queue = (int*)malloc(numCourses * sizeof(int));
+    int front = 0, rear = 0;
+
+    for (int i = 0; i < numCourses; i++) {
+        if (indegree[i] == 0)
+            queue[rear++] = i;
+    }
+
+    int* result = (int*)malloc(numCourses * sizeof(int));
+    int count = 0;
+
+    while (front < rear) {
+        int node = queue[front++];
+        result[count++] = node;
+
+        for (int i = 0; i < adjSize[node]; i++) {
+            int next = adj[node][i];
+            indegree[next]--;
+            if (indegree[next] == 0)
+                queue[rear++] = next;
+        }
+    }
+
+    if (count != numCourses) {
+        *returnSize = 0;
+        return (int*)malloc(0);
+    }
+
+    *returnSize = count;
+    return result;
+}`
+  },
   
   
 }
