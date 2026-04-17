24631da1117cc256d99a51598be3e2bd65c4dddf
Akiicodes <akshit.sinha1356@gmail.com>
2026-04-17 15:21:04 +0530

    Day 76 Solution

--- data/solutions.ts
+++ data/solutions.ts
@@ -8021,4 +8021,122 @@ bool isBipartite(int** graph, int graphSize, int* graphColSize) {
     return true;
 }`
   },
+  76: {
+    github: `/*Problem Statement
+Using DFS or BFS, count number of connected components.
+
+Input Format
+n m
+edges
+
+Output Format
+Number of connected components.
+
+Sample Input
+6 3
+1 2
+2 3
+5 6
+
+Sample Output
+3
+
+Explanation
+Components: {1,2,3}, {4}, {5,6}*/
+//Solution:
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int v;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->v = v;
+    node->next = NULL;
+    return node;
+}
+
+void dfs(int node, struct Node* adj[], int visited[]) {
+    visited[node] = 1;
+
+    struct Node* temp = adj[node];
+    while (temp) {
+        if (!visited[temp->v])
+            dfs(temp->v, adj, visited);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    struct Node* adj[n + 1];
+    for (int i = 1; i <= n; i++)
+        adj[i] = NULL;
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+
+        struct Node* n1 = createNode(v);
+        n1->next = adj[u];
+        adj[u] = n1;
+
+        struct Node* n2 = createNode(u);
+        n2->next = adj[v];
+        adj[v] = n2;
+    }
+
+    int visited[n + 1];
+    for (int i = 1; i <= n; i++)
+        visited[i] = 0;
+
+    int count = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i, adj, visited);
+            count++;
+        }
+    }
+
+    printf("%d", count);
+    return 0;
+}
+`,
+    leetcode: `#include <stdlib.h>
+
+struct Node* map[101];
+
+struct Node* dfs(struct Node* node) {
+    if (!node) return NULL;
+
+    if (map[node->val])
+        return map[node->val];
+
+    struct Node* clone = (struct Node*)malloc(sizeof(struct Node));
+    clone->val = node->val;
+    clone->numNeighbors = node->numNeighbors;
+    clone->neighbors = (struct Node**)malloc(sizeof(struct Node*) * node->numNeighbors);
+
+    map[node->val] = clone;
+
+    for (int i = 0; i < node->numNeighbors; i++) {
+        clone->neighbors[i] = dfs(node->neighbors[i]);
+    }
+
+    return clone;
+}
+
+struct Node *cloneGraph(struct Node *s) {
+    for (int i = 0; i <= 100; i++)
+        map[i] = NULL;
+
+    return dfs(s);
+}`
+  },
 }
