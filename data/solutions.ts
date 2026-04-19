eb53f0a131cf9f70d53ffc10acd6ae3795d237be
Akiicodes <akshit.sinha1356@gmail.com>
2026-04-19 19:13:28 +0530

    Day 78 Solution

--- data/solutions.ts
+++ data/solutions.ts
@@ -8294,4 +8294,166 @@ int** criticalConnections(int n, int** connections, int connectionsSize,
     return res;
 }`
   },
+  78: {
+    github: `/*Problem Statement
+Given weighted undirected graph, compute total weight of Minimum Spanning Tree using Prim’s algorithm.
+
+Input Format
+n m
+u v w
+...
+
+Output Format
+Total weight of MST.
+
+Sample Input
+4 5
+1 2 3
+1 3 5
+2 3 1
+2 4 4
+3 4 2
+
+Sample Output
+6
+
+Explanation
+One possible MST edges: (2-3), (3-4), (1-2)Problem Statement
+Given weighted undirected graph, compute total weight of Minimum Spanning Tree using Prim’s algorithm.
+
+Input Format
+n m
+u v w
+...
+
+Output Format
+Total weight of MST.
+
+Sample Input
+4 5
+1 2 3
+1 3 5
+2 3 1
+2 4 4
+3 4 2
+
+Sample Output
+6
+
+Explanation
+One possible MST edges: (2-3), (3-4), (1-2)*/
+//Solution:
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 1000
+
+int minKey(int key[], int mstSet[], int n) {
+    int min = INT_MAX, idx = -1;
+    for (int i = 1; i <= n; i++) {
+        if (!mstSet[i] && key[i] < min) {
+            min = key[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int graph[MAX][MAX];
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            graph[i][j] = 0;
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        graph[u][v] = w;
+        graph[v][u] = w;
+    }
+
+    int key[MAX], mstSet[MAX];
+
+    for (int i = 1; i <= n; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = 0;
+    }
+
+    key[1] = 0;
+
+    int total = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int u = minKey(key, mstSet, n);
+        mstSet[u] = 1;
+        total += key[u];
+
+        for (int v = 1; v <= n; v++) {
+            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    printf("%d", total);
+    return 0;
+}
+`,
+    leetcode: `class Solution {
+public:
+    int timer;
+
+    void dfs(int u, int parent, vector<int> adj[], vector<int>& vis,
+             vector<int>& disc, vector<int>& low, vector<int>& mark) {
+        
+        vis[u] = 1;
+        disc[u] = low[u] = timer++;
+        int children = 0;
+
+        for (auto v : adj[u]) {
+            if (v == parent) continue;
+
+            if (!vis[v]) {
+                children++;
+                dfs(v, u, adj, vis, disc, low, mark);
+
+                low[u] = min(low[u], low[v]);
+
+                if (parent != -1 && low[v] >= disc[u])
+                    mark[u] = 1;
+            } 
+            else {
+                low[u] = min(low[u], disc[v]);
+            }
+        }
+
+        if (parent == -1 && children > 1)
+            mark[u] = 1;
+    }
+
+    vector<int> articulationPoints(int V, vector<int> adj[]) {
+        vector<int> vis(V, 0), disc(V), low(V), mark(V, 0);
+        timer = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i])
+                dfs(i, -1, adj, vis, disc, low, mark);
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < V; i++) {
+            if (mark[i]) ans.push_back(i);
+        }
+
+        if (ans.size() == 0)
+            return {-1};
+
+        return ans;
+    }
+};`
+  },
 }
