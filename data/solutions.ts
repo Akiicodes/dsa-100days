5dde4b2e51de5ed0e901bdc8c756151e3538b3a1
Akiicodes <akshit.sinha1356@gmail.com>
2026-04-25 22:57:35 +0530

    Day 84 Solution

--- data/solutions.ts
+++ data/solutions.ts
@@ -9027,4 +9027,65 @@ int main() {
     return -1;
 }`
   },
+  84: {
+    github: `/*Problem: Implement Insertion Sort - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64*/
+//Solution:
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
+`,
+    leetcode: `int findPeakElement(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] < nums[mid + 1])
+            left = mid + 1;
+        else
+            right = mid;
+    }
+
+    return left;
+}`
+  },
 }
